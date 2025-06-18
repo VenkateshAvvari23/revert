927c33d589e5b7eddd1cf47e3c2004275734154f
VenkateshAvvari23 <venkyavvari.99@gmail.com>
2025-06-18 06:58:11 +0530

    for loop same

--- Forloop.java
+++ Forloop.java
@@ -2,6 +2,8 @@ public class ForLoopExample {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Number: " + i);
+            System.out.println("Number: " + i);
+System.out.println("Number: " + i);
         }
     }
 }
